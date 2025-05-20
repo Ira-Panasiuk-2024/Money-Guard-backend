3e05194c5a385c4828f58945e762351d8e452275
Ira Panasiuk <panasykira2022@gmail.com>
2025-05-20 11:38:49 +0300

    update file

--- src/routers/auth.js
+++ src/routers/auth.js
@@ -8,14 +8,12 @@ import {
 
 import { registerUserSchema, loginUserSchema } from '../validation/auth.js';
 
-import { authenticate } from '../middlewares/authenticate.js';
 import { validateBody } from '../middlewares/validateBody.js';
 
 import { ctrlWrapper } from '../utils/ctrlWrapper.js';
 
 const authRouter = Router();
 
-authRouter.use(authenticate);
 
 authRouter.post(
   '/register',
